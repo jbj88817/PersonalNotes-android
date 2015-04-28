9b733e6d8a56a0b3b26f8d00c21fa5de10b02677
Bojie Jiang <jiangbojie@gmail.com>
2015-04-27 21:03:41 -0700

    Drawer setup

---
+++ app/src/main/java/com/bojie/personalnotes/NavigationDrawerFragment.java
@@ -0,0 +1,66 @@
+package com.bojie.personalnotes;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by bojiejiang on 4/27/15.
+ */
+public class NavigationDrawerFragment extends Fragment {
+
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+
+    private boolean mUserLearnedDrawer;
+    private boolean mFromSavedInstanceState;
+
+    public NavigationDrawerFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mUserLearnedDrawer = Boolean.valueOf(AppSharedPreferences.hasUserLearned(getActivity(),
+                AppConstant.KEY_USER_LEARNED_DRAWER, AppConstant.FALSE));
+        if (savedInstanceState != null) {
+            mFromSavedInstanceState = true;
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+    }
+
+    public void setUp(int fragmentId, DrawerLayout drawerLayout, final Toolbar toolbar) {
+        View containerView = getActivity().findViewById(fragmentId);
+        mDrawerLayout = drawerLayout;
+        mDrawerToggle = new ActionBarDrawerToggle(getActivity(), drawerLayout,
+                toolbar, R.string.drawer_open, R.string.drawer_close){
+
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                super.onDrawerSlide(drawerView, slideOffset);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+
+        };
+    }
+}
