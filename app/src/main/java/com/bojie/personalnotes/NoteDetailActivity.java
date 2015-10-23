c4234bc3c5e9b58aa75f5ba145b3d10b79d22512
Bojie Jiang <jiangbojie@gmail.com>
2015-10-22 23:22:42 -0700

    add some

--- app/src/main/java/com/bojie/personalnotes/NoteDetailActivity.java
+++ app/src/main/java/com/bojie/personalnotes/NoteDetailActivity.java
@@ -1,5 +1,11 @@
 package com.bojie.personalnotes;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import com.dropbox.client2.DropboxAPI;
 import com.dropbox.client2.android.AndroidAuthSession;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -13,18 +19,75 @@ public class NoteDetailActivity extends BaseActivity
         implements GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener {
 
-        private static int sMonth, sHour, sDay, sMinute, sSecond;
+    private static int sMonth, sHour, sDay, sMinute, sSecond;
 
-        private DropboxAPI<AndroidAuthSession> mAPI;
-        private File mDropBoxFile;
-        private String mCameraFileName;
-        private NoteCustomList mNoteCustomList;
+    private DropboxAPI<AndroidAuthSession> mAPI;
+    private File mDropBoxFile;
+    private String mCameraFileName;
+    private NoteCustomList mNoteCustomList;
 
-        // Constants
-        public static final int NORMAL = 1;
-        public static final int LIST = 2;
-        public static final int CAMERA_REQUEST = 1888;
-        public static final int TAKE_GALLERY_CODE = 1;
+    // Constants
+    public static final int NORMAL = 1;
+    public static final int LIST = 2;
+    public static final int CAMERA_REQUEST = 1888;
+    public static final int TAKE_GALLERY_CODE = 1;
 
 
+    private static int sMonth, sYear, sHour, sDay, sMinute, sSecond;
+    private static TextView sDateTextView, sTimeTextView;
+    private static boolean sIsInAuth;
+    private static String sTmpFlNm;
+    private DropboxAPI<AndroidAuthSession> mApi;
+    private File mDropBoxFile;
+    private String mCameraFileName;
+    private NoteCustomList mNoteCustomList;
+    private EditText mTitleEditText, mDescriptionEditText;
+    private ImageView mNoteImage;
+    private String mImagePath = AppConstant.NO_IMAGE;
+    private String mId;
+    private boolean mGoingToCameraOrGallery = false, mIsEditing = false;
+    private boolean mIsImageSet = false;
+    private boolean mIsList = false;
+    private Bundle mBundle;
+    private ImageView mStorageSelection;
+    private boolean mIsNotificationMode = false;
+    private String mDescription;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        this.mBundle = savedInstanceState;
+        setContentView(R.layout.activity_detail_note_layout);
+        activateToolbarWithHomeEnabled();
+        if (getIntent().getStringExtra(AppConstant.LIST_NOTES) != null) {
+            initializeComponents(LIST);
+        } else {
+            initializeComponents(NORMAL);
+        }
+
+        setUpIfEditing();
+        if (getIntent().getStringExtra(AppConstant.GO_TO_CAMERA) != null) {
+            callCamera();
+        }
+    }
+
+    private void setUpIfEditing() {
+        if (getIntent().getStringExtra(AppConstant.ID) != null) {
+            mId = getIntent().getStringExtra(AppConstant.ID);
+            mIsEditing = true;
+            if (getIntent().getStringExtra(AppConstant.LIST_NOTES) != null) {
+                initializeComponents(LIST);
+            }
+            setValues(mId);
+            mStorageSelection.setEnabled(false);
+            if (getIntent().getStringExtra(AppConstant.REMINDER) != null) {
+                Note aNote = new Note(getIntent().getStringExtra(AppConstant.REMINDER));
+                mId = aNote.getId() + "";
+                mIsNotificationMode = true;
+                setValues(aNote);
+                removeFromReminder(aNote);
+                mStorageSelection.setEnabled(false);
+            }
+        }
+    }
 }
